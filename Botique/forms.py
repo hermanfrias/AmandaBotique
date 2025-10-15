3ec99575797c914e880d3b2595778d1ce15ff337
HERMAN FRIAS <hermanfrias@hotmail.com>
2025-10-15 18:41:41 -0400

    Se agregarno la carga de infromacion en las bd y las vista de listados

---
+++ Botique/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from .models import Cliente, Catalogo, Proveedor
+
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+        fields = ['identificacion','nombre', 'apellido', 'email', 'telefono', 'direccion', 'fecha_nacimiento']
+        Widget = {
+            "identificacion": forms.TextInput(attrs={'class':'form-control'}),
+            "nombre": forms.TextInput(attrs={'class':'form-control'}),
+            "apellido": forms.TextInput(attrs={'class':'form-control'}),
+            "email": forms.EmailInput(attrs={'class':'form-control'}),
+            "telefono": forms.TextInput(attrs={'class':'form-control'}), 
+            "direccion": forms.Textarea(attrs={'class':'form-control'}),
+            "fecha_nacimiento": forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),        
+        }
+        
+class CatalogoForm(forms.ModelForm):
+    class Meta:
+        model = Catalogo
+        fields = ['codigo','nombre', 'descripcion', 'precio']
+        Widget = {
+            "codigo": forms.TextInput(attrs={'class':'form-control'}),
+            "nombre": forms.TextInput(attrs={'class':'form-control'}),
+            "descripcion": forms.TextInput(attrs={'class':'form-control'}),
+            "precio": forms.NumberInput(attrs={'class':'form-control'}),
+        }
+        
+class ProveedorForm(forms.ModelForm):
+    class Meta:
+        model = Proveedor
+        fields = ['nombre', 'contacto', 'email', 'telefono', 'direccion']
+        Widget = {
+            "nombre": forms.TextInput(attrs={'class':'form-control'}),
+            "contacto": forms.TextInput(attrs={'class':'form-control'}),
+            "email": forms.EmailInput(attrs={'class':'form-control'}),
+            "telefono": forms.TextInput(attrs={'class':'form-control'}),
+            "direccion": forms.Textarea(attrs={'class':'form-control'}),
+        }
+        
--- Botique/views.py
+++ Botique/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ClienteForm, CatalogoForm, ProveedorForm
 from .models import Cliente, Catalogo, Proveedor
 
 # Create your views here.
@@ -6,29 +7,62 @@ def index(request):
     return render(request, "Botique/index.html")
 
 def actualizar_catalogo(request):
-    # Lógica para actualizar el catálogo
-    return render(request, "Botique/actualizar_catalogo.html")
+    if request.method == 'POST':
+        form = CatalogoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("actualizar_catalogo")
+    else:
+            form = CatalogoForm()
+        
+    return render(request, "Botique/actualizar_catalogo.html", {'form': form})
 
 def actualizar_clientes(request):
-    # Lógica para actualizar los clientes
-    return render(request, "Botique/actualizar_clientes.html")
+    if request.method == 'POST':
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("actualizar_clientes")
+    else:
+            form = ClienteForm()
+            
+    return render(request, "Botique/actualizar_clientes.html", {'form': form})
 
 def actualizar_proveedores(request):
-    # Lógica para actualizar los proveedores
-    return render(request, "Botique/actualizar_proveedores.html")
+    if request.method == 'POST':
+        form = ProveedorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("actualizar_proveedores")
+    else:
+            form = ProveedorForm()
+            
+    return render(request, "Botique/actualizar_proveedores.html", {'form': form})
 
 # --- LISTAR BASES DE DATOS ---
 
 def listar_catalogo(request):
-    catalogo = Catalogo.objects.all()
-    return render(request, 'Botique/listar_catalogo.html', {'productos': catalogo})
+    query = request.GET.get('buscar', '')
+    if len(query) > 0:
+        catalogos = Catalogo.objects.filter(nombre__icontains=query).order_by('codigo')
+    else:       
+        catalogos = Catalogo.objects.all()
+    return render(request, 'Botique/listar_catalogo.html', {'catalogos': catalogos})
 
 def listar_clientes(request):
-    clientes = Cliente.objects.all()
+    query = request.GET.get('buscar', '')
+    if len(query) > 0:
+        clientes = Cliente.objects.filter(nombre__icontains=query).order_by('identificacion ')
+    else:       
+        clientes = Cliente.objects.all()
     return render(request, 'Botique/listar_clientes.html', {'clientes': clientes})
 
 def listar_proveedores(request):
-    proveedores = Proveedor.objects.all()
+    query = request.GET.get('buscar', '')
+    if len(query) > 0:
+        proveedores = Proveedor.objects.filter(nombre__icontains=query).order_by('nombre')
+    else:       
+        proveedores = Proveedor.objects.all()
     return render(request, 'Botique/listar_proveedores.html', {'proveedores': proveedores})
 
 
