2abb0c5f2ef42bb32bdba1802f3c89d7e2a14e70
HERMAN FRIAS <hermanfrias@hotmail.com>
2025-11-03 15:08:20 -0400

    campo de imagen obligatorio

--- BoutiqueApp/models.py
+++ BoutiqueApp/models.py
@@ -1,16 +1,14 @@
 from django.db import models
 
 class Catalogo(models.Model):
-    codigo = models.CharField(max_length=20, unique=True)
+    codigo = models.CharField(max_length=20)
     modelo = models.CharField(max_length=100)
     estilo = models.CharField(max_length=100)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-    imagen_modelo = models.ImageField(upload_to = 'imagenes', null=True, blank = True)
+    imagen_modelo = models.ImageField(upload_to = 'imagenes', null=True)
         
     def __str__(self):
         return f"{self.codigo}: {self.modelo} - {self.estilo}"
-    
 
-    
 
