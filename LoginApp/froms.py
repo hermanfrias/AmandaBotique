df0d1c081c27d0f73d097b726dca3a5de68ee205
HERMAN FRIAS <hermanfrias@hotmail.com>
2025-11-04 12:51:08 -0400

    Se hicieron unos ajustes visuales el LoginApp

--- LoginApp/froms.py
+++ LoginApp/froms.py
@@ -10,19 +10,26 @@ class FormularioCreacionUsuario(UserCreationForm):
 class FormularioCambioUsuario(UserChangeForm):
     class Meta:
         model = PerfilUsuario
-        fields = ('avatar', 'ciudad', 'pais', 'telefono', 'fecha_nacimiento', 'first_name', 'last_name', 'password', 'username')
-        
-        Widget = {
+        fields = (
+            'avatar', 'ciudad', 'pais', 'telefono',
+            'fecha_nacimiento', 'first_name', 'last_name',
+            'password', 'username'
+        )
+        widgets = {  # 👈 aquí va en minúsculas
             "avatar": forms.ClearableFileInput(attrs={"class": "form-control"}),
             "pais": forms.TextInput(attrs={"class": "form-control", "placeholder": "País"}),
             "ciudad": forms.TextInput(attrs={"class": "form-control", "placeholder": "Ciudad"}),
             "first_name": forms.TextInput(attrs={"class": "form-control", "placeholder": "Nombre"}),
             "last_name": forms.TextInput(attrs={"class": "form-control", "placeholder": "Apellido"}),
             "email": forms.EmailInput(attrs={"class": "form-control", "placeholder": "Email"}),
-            "fecha_nacimiento": forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            "fecha_nacimiento": forms.DateInput(
+                format='%Y-%m-%d',
+                attrs={
+                    'type': 'date',
+                    'class': 'form-control',
+                    'id': 'id_fecha_nacimiento'
+                }
+            ),
             "username": forms.TextInput(attrs={"class": "form-control", "placeholder": "Nombre de usuario"}),
             "password": forms.PasswordInput(attrs={"class": "form-control", "readonly": "readonly"}),
         }
-        
-        
-        
--- LoginApp/models.py
+++ LoginApp/models.py
@@ -8,9 +8,9 @@ def cargar_avatar(instance, filename):
 class PerfilUsuario(AbstractUser):
     avatar = models.ImageField(upload_to = cargar_avatar, default = "default/default_icono.png", null=True, blank=True, verbose_name = "Avatar")
     email = models.EmailField(unique=True)
-    ciudad = models.CharField(max_length=100, null=True, blank=True)
-    pais = models.CharField(max_length=100, null=True, blank=True)
-    telefono = models.CharField(max_length=20, null=True, blank=True)
+    ciudad = models.CharField(max_length=100, null=True)
+    pais = models.CharField(max_length=100, null=True)
+    telefono = models.CharField(max_length=20, null=True)
     fecha_nacimiento = models.DateField(null=True, blank=True)
     
     class Meta:
