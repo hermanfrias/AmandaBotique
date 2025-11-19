e1df29fd26143fa5b173dcfd2a95320261aa1364
HERMAN FRIAS <hermanfrias@hotmail.com>
2025-11-19 16:51:02 -0400

    css arreglo

--- AmandaProjecto/settings.py
+++ AmandaProjecto/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -132,7 +133,6 @@ AUTH_USER_MODEL ='LoginApp.PerfilUsuario'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-# MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'perfil'
